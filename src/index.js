67802f935f1cf67ee0be69bfc6b0b84a42143e2f
Romário Lima <devromariolima@gmail.com>
2024-03-11 22:25:17 -0300

    alteração da rota

--- src/index.js
+++ src/index.js
@@ -13,6 +13,6 @@ function GerarDados(req, res) {
     });
 }
 
-app.get('/.api-node-romario-lima.netlify.app/api', GerarDados);
+app.get('/api', GerarDados);
 
 app.listen(3333, () => console.log('executando'));
